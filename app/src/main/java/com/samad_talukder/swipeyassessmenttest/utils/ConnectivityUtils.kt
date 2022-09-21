5e76d6954923d2efb22e72baf3d9124b8e4a69cf
Samad Talukder <samadtalukdar6@gmail.com>
2022-09-21 23:28:27 +0800

    Created ConnectivityUtils objet for checking has internet connection available

---
+++ app/src/main/java/com/samad_talukder/swipeyassessmenttest/utils/ConnectivityUtils.kt
@@ -0,0 +1,28 @@
+package com.samad_talukder.swipeyassessmenttest.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+
+object ConnectivityUtils {
+
+    fun hasInternetConnection(context: Context?): Boolean {
+        if (context == null)
+            return false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities =
+            connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+
+}
